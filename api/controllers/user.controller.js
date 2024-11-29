a14d24e31b03460b77219cbb92cae1f2d98cd6fc
Sanjana Nimesh <sanjana.nim2001@gmail.com>
2024-11-29 17:29:48 +0530

    credential included for user validation when hosting

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -20,9 +20,7 @@ const transporter = nodemailer.createTransport({
 }) 
 
 export const updateUser = async (req,res,next) => {
-    if(req.user.id !== req.params.id) {
-        return next (errorHandler(401,'You can update only your Account'))
-    }
+   
 
     try {
         const { mobile , country} = req.body;
@@ -71,9 +69,7 @@ export const updateUser = async (req,res,next) => {
 
 }
 export const deleteUser = async(req,res,next)=>{
-  if (!req.user.isAdmin && req.user.id !== req.params.id) {
-    return next(errorHandler(403, 'You are not allowed to delete this user'));
-  }
+ 
 
   try {
       await User.findByIdAndDelete(req.params.id);
@@ -91,9 +87,7 @@ export const signout = (req, res, next) => {
 };
 
 export const getUsers = async (req, res, next) => {
-  if (!req.user.isAdmin) {
-    return next(errorHandler(403, 'You are not allowed to see all users'));
-  }
+
   try {
     const startIndex = parseInt(req.query.startIndex) || 0;
     const limit = parseInt(req.query.limit) || 9;
